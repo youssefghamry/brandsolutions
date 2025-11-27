3372830e646967f4baaae60bb8a5681b7fb5bf46
youssefghamry <77971093+youssefghamry@users.noreply.github.com>
2025-11-27 23:17:39 +0200

    1

---
+++ foodking/assets/lib/slickslider/slick.init.js
@@ -0,0 +1,11 @@
+$('.ini-testimonial-slider').slick({
+    vertical: true,
+    autoplay: true,
+    centerMode: true,
+    arrows: true,
+    speed: 1e3,
+    autoplaySpeed: 2500,
+    slidesToShow: 1,
+    prevArrow: '<i class="sl sl-icon-arrow-left slick-prev"></i>',
+    nextArrow: '<i class="sl sl-icon-arrow-right slick-next"></i>',
+});
